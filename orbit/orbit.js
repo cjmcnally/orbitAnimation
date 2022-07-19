32d4b4f6fbac7a4a43a0556256c76232c6edb3f0
Colin <99578101+ispyc@users.noreply.github.com>
2022-07-19 00:30:06 -0400

    Centered everything, fixed rotation speeds and trajectory, added another object.
    I coded it so across different displays everything would stay centered. I fixed the rotation speeds and trajectory of the objects, by increasing the angles at which they rotated around the center point. I also added another object, and changed the rotation speeds + trajectory of said object.

--- orbit/orbit.js
+++ orbit/orbit.js
@@ -7,14 +7,19 @@ canvas.height = window.innerHeight;
 // ctx.arc(x-coord, y-coord, radius, start-angle, end-angle)
 
 // Sets the x, y and angle to specific amount / coordinate
-let positionY = 400;
-let positionX = 710;
+let x = canvas.width/2.5;
+let y = canvas.height/2;
 let angle = 0;
 
 // Sets the x, y and angle to specific amount / coordinate
-let x = 680;
-let y = 390;
+let x2 = canvas.width/3;
+let y2 = canvas.height/2;
 let angle2 = 0;
+
+// Sets the x, y and angle to specific amount / coordinate
+let x3 = canvas.width/3.25;
+let y3 = canvas.height/2;
+let angle3 = 0;
 
 function drawStars() {
     // Big star
@@ -22,7 +27,7 @@ function drawStars() {
     ctx.strokeStyle = 'orangered';
     ctx.lineWidth = 2;
     ctx.beginPath();
-    ctx.arc(900, 400, 100, 0, Math.PI * 2);
+    ctx.arc(canvas.width/2, canvas.height/2, 90, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
@@ -32,43 +37,55 @@ function drawStars() {
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 2;
     ctx.beginPath();
-    ctx.arc(positionX, positionY, 17, 0, Math.PI * 2);
+    ctx.arc(x, y, 15, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke(); 
 
-    // Outer-most star
+    // 2nd closest to big star
     ctx.fillStyle = 'whitesmoke';
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 2;
     ctx.beginPath();
-    ctx.arc(x, y, 13, 0, Math.PI * 2);
+    ctx.arc(x2, y2, 17, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
 
+    // Outer-most
+    ctx.fillStyle = 'whitesmoke';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.arc(x3, y3, 20, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
 }
-animate();
 
 // Animation Function
 function animate(){
-
     // Clears after images
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Changes angle & direction of tiny stars / makes the stars orbit main star
-    positionX += 15 * Math.sin(angle);
-    positionY += 15 * Math.cos(angle);
+    x += 3 * Math.sin(angle);
+    y += 3 * Math.cos(angle);
 
-    x += 10 * Math.sin(angle2);
-    y += 10 * Math.cos(angle2);
+    x2 += 5 * Math.sin(angle2);
+    y2 += 5 * Math.cos(angle2);
+
+    x3 += 7 * Math.sin(angle3);
+    y3 += 7 * Math.cos(angle3);
 
     // Changes how long the stars orbit around the main star
-    angle += 0.07;
-    angle2 += 0.042;
+    angle += 0.015;
+    angle2 += 0.017;
+    angle3 += 0.019;
 
     // Calls the drawStars Function
     drawStars();
     // Requests animation to current function
     requestAnimationFrame(animate);
 }
+animate();
